7c6b751eede968916975f9e07405c1309c569069
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-08-08 21:53:24 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+// Test 1
+const num1 = 31, num2 = 2;
+const multiply = num1 * num2;
+
+// Test 2
+const random = Number.parseInt(Math.random() * 10) + 1;
+
+// Test 3
+const num3 = 59, num4 = 5;
+const mod = num3 % num4;
+
+// Test 4
+const max = Math.max(20, 16, 4, 14, 0);
